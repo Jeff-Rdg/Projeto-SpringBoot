8342253b1ab93cf01483fbd0b5fa4674187b8dd9
Jefferson Rodrigues <jsilvardg@outlook.com>
2021-09-12 16:06:47 -0300

    Atualizando

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dsvendas.entities;
+
+public class Sale {
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	private seller Seller;
+
+}
